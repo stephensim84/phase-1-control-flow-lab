4aa499f065fc7622d4cd9bfb9370bf93c3dd02ef
Stephen Simmons <stephensimmons@stephens-MacBook-Pro.local>
2022-03-13 20:58:48 -0700

    commit tree d16884be00a8eb17e00f1554c724ea7bc37983e7

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(feet){
+  if (feet <= 400){
+     return "This one is on me!";
+   }  else if (feet > 2500) {
+       return "No can do.";
+   } else {
+     return "I will gladly take your thirty bucks.";
+    } 
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  function ternaryCheckCity(city) {
+
+    let test = city === "NYC" ? "Ok, sounds good." : "No go.";
+
+    return test;
+
+  }
+
+  function switchOnCharmFromTip(tip){
+
+
+  switch(tip) {
+    case "generous" :
+       return "Thank you so much.";
+    case "not as generous":
+     return "Thank you.";
+    default: 
+      return "Bye."
+  }
+  
+  }
